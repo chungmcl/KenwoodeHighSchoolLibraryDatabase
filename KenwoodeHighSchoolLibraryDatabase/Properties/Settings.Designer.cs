2293ab1a095023f00dd3d2f84af9776cb1ee148e
Micheal Chung 鍾俊豪 <micheal.012@live.com>
2018-01-14 19:33:01 -0800

    Connected to LibraryDatabase and added basic registration window XAML

--- KenwoodeHighSchoolLibraryDatabase/Properties/Settings.Designer.cs
+++ KenwoodeHighSchoolLibraryDatabase/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KenwoodeHighSchoolLibraryDatabase.Properties
-{
-
-
+namespace KenwoodeHighSchoolLibraryDatabase.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\LibraryDatabase.mdb;" +
+            "Persist Security Info=True;User ID=admin")]
+        public string LibraryDatabaseConnectionString {
+            get {
+                return ((string)(this["LibraryDatabaseConnectionString"]));
             }
         }
     }
