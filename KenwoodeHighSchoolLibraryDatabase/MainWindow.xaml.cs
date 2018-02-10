c3ad2a0e53f2dd1e462620f4dfbef7489a17b10b
Micheal Chung 鍾俊豪 <micheal.012@live.com>
2018-02-09 21:14:01 -0800

    Figured out querying from database and loading info into dataGrid!

--- KenwoodeHighSchoolLibraryDatabase/MainWindow.xaml.cs
+++ KenwoodeHighSchoolLibraryDatabase/MainWindow.xaml.cs
@@ -30,20 +30,32 @@ namespace KenwoodeHighSchoolLibraryDatabase
             c = new OleDbConnection();
             c.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|" +
                 "\\LibraryDatabase.mdb;Persist Security Info=True;User ID=admin";
+            OleDbDataReader reader = null;
+            c.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = c;
+            command.CommandText = "SELECT * FROM accounts";
+            command.CommandType = System.Data.CommandType.Text;
 
-            //OleDbDataAdapter adapter = new OleDbDataAdapter();
-            //DataTable dTable = new DataTable();
-            //adapter.Fill(dTable);
-            //this.dataGridAccounts.DataSource = dTable;
+            reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                User newUser = new User();
+                newUser.firstName = reader["firstName"].ToString();
+                newUser.lastName = reader["lastName"].ToString();
+                newUser.userID = reader["userID"].ToString();
+                newUser.userType = reader["userType"].ToString();
+                newUser.bookLimit = reader["bookLimit"].ToString();
+                newUser.dateLimit = reader["dateLimit"].ToString();
+                dataGridAccounts.Items.Add(newUser);
+            }
+            c.Close();
         }
 
         private void BtnToRegistrationWindow_Click(object sender, RoutedEventArgs e)
         {
             RegistrationWindow w = new RegistrationWindow();
             w.Show();
-
-            dataGridAccounts.Items.Add("hehexD");
-
         }
 
         private void TstBtnDeleteFromAccounts_Click(object sender, RoutedEventArgs e)
@@ -53,9 +65,20 @@ namespace KenwoodeHighSchoolLibraryDatabase
             OleDbCommand command = new OleDbCommand();
             command.Connection = c;
 
+            c.Open();
             command.CommandText = "DELETE * FROM accounts";
             command.ExecuteNonQuery();
             c.Close();
         }
+    }
+
+    public struct User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userID { get; set; }
+        public string userType { get; set; }
+        public string bookLimit { get; set; }
+        public string dateLimit { get; set; }
     }
 }
